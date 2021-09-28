6d993a045f008278e38bea9ad74aa08d8e7ce862
yunpeng <ypjiang@cs.hku.hk>
2021-09-28 16:22:15 +0800

    update KVRead

--- vendor/github.com/Yunpeng-J/fabric-protos-go/ledger/rwset/kvrwset/kv_rwset.pb.go
+++ vendor/github.com/Yunpeng-J/fabric-protos-go/ledger/rwset/kvrwset/kv_rwset.pb.go
@@ -146,6 +146,8 @@ func (m *HashedRWSet) GetMetadataWrites() []*KVMetadataWriteHash {
 type KVRead struct {
 	Key                  string   `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
 	Version              *Version `protobuf:"bytes,2,opt,name=version,proto3" json:"version,omitempty"`
+	Value                []byte   `protobuf:"bytes,3,opt,name=value,proto3" json:"value,omitempty"`
+	Txid                 string   `protobuf:"bytes,4,opt,name=txid,proto3" json:"txid,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
